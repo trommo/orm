79b55e7ab8357a01b417c7f7a40a2658f9ab346a
Vitaliy Tretyakov <ot3@ukr.net>
2020-01-24 13:11:34 +0200

    Javadoc annotations added

--- src/main/java/annotation/ManyToOneSoft.java
+++ src/main/java/annotation/ManyToOneSoft.java
@@ -6,7 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Аннотация для связи many-to-one
+ * Many-to-one relationship annotation
+ * 
+ * @author Anna Severyna
  */
 
 @Target(ElementType.FIELD)
