37d693bd556775157d49b6edcee5d41cef3ba555
Vitaliy Tretyakov <ot3@ukr.net>
2020-01-24 12:32:01 +0200

    Javadoc annotations added

--- src/main/java/mapping/TypeConverter.java
+++ src/main/java/mapping/TypeConverter.java
@@ -4,8 +4,21 @@ import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Class converts data types
+ *
+ * @author Anna Severyna
+ */
 public class TypeConverter {
 
+    /**
+     * Method converts data types from database to java format
+     *
+     * @param   object      object name
+     * @param   field       field name
+     * @param   resultSet   incoming data set from database
+     * @param   colNumber   column number
+     */
     public void convertDataSqlToJava(Object object, Field field, ResultSet resultSet, int colNumber) {
         Class<?>fieldType = field.getType();
         int columnTypeNumber = 0;
@@ -39,6 +52,14 @@ public class TypeConverter {
         }
     }
 
+    /**
+     * Method checks do the data type from database matches the data type in specified class
+     *
+     * @param   colNumber       column number
+     * @param   dataBaseType    data type in database
+     * @param   fieldType       data type of corresponding field in specified class
+     * @return  true if the data type from the database matches the data type in the specified class
+     */
     private boolean isSameType(int colNumber, DataBaseType dataBaseType, Class<?> fieldType) {
         return colNumber == dataBaseType.getTypeNumber() && (fieldType.isAssignableFrom(dataBaseType.getClassWrapper())) || fieldType.getName().equals(dataBaseType.getNameJavaTypePrimitive());
     }
