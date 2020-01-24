5482bdbb3e488b476897dc7ccf0e96b95e84c611
Vitaliy Tretyakov <ot3@ukr.net>
2020-01-24 07:52:49 +0200

    Javadoc annotations added

--- src/main/java/crud/CRUDUtilities.java
+++ src/main/java/crud/CRUDUtilities.java
@@ -4,8 +4,19 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.StringJoiner;
 
+/**
+ *  Class contains auxiliary methods for create and update methods
+ *
+ * @author Anna Severyna
+ *
+ */
 public class CRUDUtilities {
 
+    /**
+     *
+     * @param linkedList    list of column names of database table
+     * @return              string of column names of database table
+     */
     public static String listColumnsToString(List<String> linkedList){
         Iterator<String> listIter = linkedList.iterator();
         StringJoiner sj = new StringJoiner(", ");
@@ -16,7 +27,11 @@ public class CRUDUtilities {
         return sj.toString().trim();
     }
 
-
+    /**
+     *
+     * @param linkedList    list of column names values from database table
+     * @return              string of column names values from database table
+     */
     public static String listValuesToString(List <String> linkedList){
         Iterator<String> listIter = linkedList.iterator();
         StringJoiner sj = new StringJoiner(", ");
