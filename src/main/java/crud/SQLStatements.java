bcabd1be006f3ff569d05b337543c4bb37c91a95
Vitaliy Tretyakov <ot3@ukr.net>
2020-01-24 13:41:27 +0200

    Javadoc annotations added

--- src/main/java/crud/SQLStatements.java
+++ src/main/java/crud/SQLStatements.java
@@ -1,5 +1,10 @@
 package crud;
 
+/**
+ * Class stores String constants for PostgreSQL statements
+ *
+ * @author Anna Severyna
+ */
 public class SQLStatements {
     public static final String DELETE_BY_ID = "DELETE FROM %s WHERE %s = %s";
     public static final String SELECT_BY_ID = "SELECT * FROM %s WHERE %s = %d";
